a9defbb0ca2b211610b74771b7f49c110a0db669
kilianrdgs <krdgs.rodrigues@gmail.com>
2026-02-22 17:59:51 +0100

    biome

--- src/domain/tests/roomManager.test.ts
+++ src/domain/tests/roomManager.test.ts
@@ -1,47 +1,51 @@
 import { describe, expect, it } from "vitest";
-import { Room } from "../room";
-import { RoomManager } from "../roomManager";
 import { Player } from "../player";
+import { RoomManager } from "../roomManager";
 
 describe("RoomManager", () => {
-  describe("createRoom", () => {
-    it("Creates a new room", () => {
-      const manager = new RoomManager();
+	describe("createRoom", () => {
+		it("Creates a new room", () => {
+			const manager = new RoomManager();
 
-      const room = manager.createRoom("1234");
+			const room = manager.createRoom("1234");
 
-      expect(room).not.toBeNull();
-      expect(manager.getRoom("1234")).toBe(room);
-    });
+			expect(room).not.toBeNull();
+			expect(manager.getRoom("1234")).toBe(room);
+		});
 
-    it("Returns null if room already exists", () => {
-      const manager = new RoomManager();
+		it("Returns null if room already exists", () => {
+			const manager = new RoomManager();
 
-      manager.createRoom("1234");
-      const secondRoom = manager.createRoom("1234");
+			manager.createRoom("1234");
+			const secondRoom = manager.createRoom("1234");
 
-      expect(secondRoom).toBeNull();
-    });
+			expect(secondRoom).toBeNull();
+		});
+	});
 
-    it("Removes room if empty", () => {
-      const manager = new RoomManager();
+	describe("removeRoomIfEmpty", () => {
+		it("Removes room if empty", () => {
+			const manager = new RoomManager();
+			const room = manager.createRoom("1234");
 
-      const room = manager.createRoom("1234");
+			if (!room) throw new Error("room should exist");
 
-      manager.removeRoomIfEmpty(room!);
+			manager.removeRoomIfEmpty(room);
 
-      expect(manager.getRoom("1234")).toBeUndefined();
-    });
+			expect(manager.getRoom("1234")).toBeUndefined();
+		});
 
-    it("Does not remove room if not empty", () => {
-      const manager = new RoomManager();
-      const room = manager.createRoom("1234");
-      const player1 = new Player("player-1", "usertest");
+		it("Does not remove room if not empty", () => {
+			const manager = new RoomManager();
+			const room = manager.createRoom("1234");
 
-      room!.addPlayer(player1);
-      manager.removeRoomIfEmpty(room!);
+			if (!room) throw new Error("room should exist");
 
-      expect(manager.getRoom("1234")).toBeDefined();
-    });
-  });
+			const player1 = new Player("player-1", "usertest");
+			room.addPlayer(player1);
+			manager.removeRoomIfEmpty(room);
+
+			expect(manager.getRoom("1234")).toBeDefined();
+		});
+	});
 });
